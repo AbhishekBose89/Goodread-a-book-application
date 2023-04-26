583e48c06bad256e1cd6dedecaf74d3a7e8f1fa6
AbhishekBose89 <abhishekbose9289@gmail.com>
2023-04-26 17:41:34 +0530

    uploaded to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+
+import AboutPage from './pages/AboutPage';
+import AddReviewPage from "./pages/AddReviewPage";
+import BookDetailPage from "./pages/BookDetailPage";
+import BooksPage from "./pages/BooksPage";
+import ContactPage from './pages/ContactPage';
+import ErrorPage from "./pages/ErrorPage"
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import ProtectedRoute from './routes/ProtectedRoute';
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path="/about" element={<ProtectedRoute component={AboutPage} />} />
+          <Route path='/books' element={<ProtectedRoute component={BooksPage} />} />
+          <Route path='/bookDetail' element={<ProtectedRoute component={BookDetailPage} />} />
+          <Route path='/addReview' element={<ProtectedRoute component={AddReviewPage} />} />
+          <Route path='/contact' element={<ProtectedRoute component={ContactPage} />} />
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/profile' element={<ProtectedRoute component={ProfilePage} />} />
+          <Route path='/*' element={<ErrorPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
